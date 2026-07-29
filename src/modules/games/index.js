e0c6414bf9d14ed8dd9f7f907e84ce37be6f9fc2
maxjurreau-3 <maxjurreau@gmail.com>
2026-07-28 21:43:01 -0400

    Update index.js

--- src/modules/games/index.js
+++ src/modules/games/index.js
@@ -1,5 +1,5 @@
 export function gamesEngine() {
   console.log('%c[Games] Engine module loaded.', 'color: #E91E63;');
 
-  // Placeholder: game registry, micro-games, runtime hooks
+  // Future: micro-games, runtime hooks, game registry
 }
