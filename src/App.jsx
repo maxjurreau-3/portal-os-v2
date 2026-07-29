8ad13b2a4ceb0012d9668471950c8d2620f76965
maxjurreau-3 <maxjurreau@gmail.com>
2026-07-28 21:42:23 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,10 @@ function App() {
           <li key={m}>{m}</li>
         ))}
       </ul>
+
+      <p style={{ marginTop: '20px', opacity: 0.7 }}>
+        Add "games" to portal.config.json to enable the Games Engine.
+      </p>
     </div>
   );
 }
