d55f13f06fac5982c4b01849c03a2baf94e789bd
maxjurreau-3 <maxjurreau@gmail.com>
2026-07-28 21:37:50 -0400

    Update boot.js

--- src/runtime/boot.js
+++ src/runtime/boot.js
@@ -7,11 +7,9 @@ export async function bootPortalOS() {
   const state = getState();
   console.log('Ecosystem State:', state);
 
-  // Load config
   const config = await fetch('/portal.config.json').then(r => r.json());
   console.log('Config Loaded:', config);
 
-  // Load modules from config
   config.modules.forEach(module => loadModule(module));
 
   console.log('%cPortal OS v2 Ready.', 'color: #2196F3; font-size: 16px;');
